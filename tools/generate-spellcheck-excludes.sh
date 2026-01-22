ff3b5413851051a144a9a41622703b09d77f512f
Jaidev Singh Chadha <jaidevsingh.chadha@arm.com>
2026-01-22 14:05:58 -0800

    Remove obsolete generate-spellcheck-excludes.sh script

--- tools/generate-spellcheck-excludes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple script to generate excludes list for draft Learning Paths
-# Usage: ./tools/generate-spellcheck-excludes.sh > excludes.txt
-
-echo "excludes:"
-find content/learning-paths -name "_index.md" -exec grep -l "^draft: true$" {} \; | while read -r file; do
-  dir=$(dirname "$file")
-  echo "  - '$dir/**/*.md'"
-done
